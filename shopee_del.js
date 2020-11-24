2c0f74e4429ecb388f689f4132f0925c9c82b54b
nel003 <37954915+nel003@users.noreply.github.com>
2020-11-24 16:24:35 +0800

    Update shopee_del.js

--- shopee_del.js
+++ shopee_del.js
@@ -4,17 +4,17 @@ var body03 = document.querySelector("body");
 var email03 = document.getElementById("email-input");
 var ranEmail = makeid(15) + "@gmail.com";
 
-function hideReasonBox() {
-  function makeid(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  
+  return result;
+}
+
+function hideReasonBox() {
   customSelect03[customSelect03.length - 1].style.display = "none";
   pageHide03.style.display = "none";
   body03.style.height = "100vh";
