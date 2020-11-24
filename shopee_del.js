e009196f0fddb3405737141c042f21c3851dc89a
nel003 <37954915+nel003@users.noreply.github.com>
2020-11-24 16:23:35 +0800

    Update shopee_del.js

--- shopee_del.js
+++ shopee_del.js
@@ -1,27 +1,22 @@
-function makeid(length) {
+var pageHide03 = document.querySelector(".page_hide");
+var customSelect03 = document.querySelectorAll(".custom_select");
+var body03 = document.querySelector("body");
+var email03 = document.getElementById("email-input");
+var ranEmail = makeid(15) + "@gmail.com";
 
-   var result           = '';
-
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-   var charactersLength = characters.length;
-
-   for ( var i = 0; i < length; i++ ) {
-
+function hideReasonBox() {
+  function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-
-   }
-
-   return result;
-
+    }
+    return result;
+  }
+  
+  customSelect03[customSelect03.length - 1].style.display = "none";
+  pageHide03.style.display = "none";
+  body03.style.height = "100vh";
+  email03.value = ranEmail;
 }
-function ShopDel() {
-   var ranEmail = makeid(15)+"@gmail.com";
-
-   document.querySelector(".page_hide").style.display="none";
-
-   document.querySelector(".custom_select").style.display="none";
-
-   document.getElementById("email-input").value = ranEmail;
-}
-
